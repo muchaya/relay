6db7b5de5972e77178f2d4c3c895e4af8e94b0fa
Blessing Muchaya <iblessing@outlook.com>
2026-01-30 09:23:12 +0200

    Feature: Add routes

---
+++ db/migrate/20260130071834_create_routes.rb
@@ -0,0 +1,12 @@
+class CreateRoutes < ActiveRecord::Migration[8.0]
+  def change
+    create_table :routes do |t|
+      t.string :from_city
+      t.string :to_city
+      t.integer :distance_km
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
