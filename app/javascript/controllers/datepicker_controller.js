c1c617162e8c9d51607cb0436d03973adf9d14af
Blessing Muchaya <iblessing@outlook.com>
2026-02-02 09:15:19 +0200

    WIP: Search filters

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -8,9 +8,12 @@ export default class extends Controller {
   static targets = ["datepicker"]
 
   connect() {
+    localeEn.daysMin = this.#daysShort()
+
     this.picker = new AirDatepicker(this.datepickerTarget, {
       locale: localeEn,
       autoClose: true,
+      inline: true,
       navTitles: {
         days: 'MMMM yyyy'
       }
@@ -19,5 +22,9 @@ export default class extends Controller {
 
   show() {
     this.picker.show()
+  }
+
+  #daysShort() {
+    return ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
   }
 }
--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -3,14 +3,46 @@ import {computePosition} from '@floating-ui/dom'
 
 // Connects to data-controller="popover"
 export default class extends Controller {
-  connect() {
-    computePosition(button, tooltip, {
-      placement: 'bottom',
-    }).then(({x, y}) => {
-      Object.assign(tooltip.style, {
-        left: `${x}px`,
-        top: `${y}px`,
-      });
-    });
+  
+  static targets = ['filtersContainer', 'popover']
+
+  open(event) {
+    this.closeOtherPopovers()
+
+    const popoverId = event.params.id
+
+    const reference = event.target
+
+    const popover = document.getElementById(popoverId)
+
+    const container = this.filtersContainerTarget
+
+    const referenceRect = reference.getBoundingClientRect()
+    const containerRect = container.getBoundingClientRect()
+
+    console.log(window.innerWidth)
+
+    const left = referenceRect.left - containerRect.left
+
+    console.log(left)
+
+    // Vertical: always below the search box
+    const top = container.offsetHeight + 8
+
+    popover.classList.remove("hidden")
+
+    Object.assign(popover.style, {
+      right: "36px",
+      top: `${top}px`,
+      left: "auto"
+    })
+  }
+
+  closeOtherPopovers() {
+    this.popoverTargets.forEach( (popover)=> {
+      if(popover !== event.target) {
+        popover.classList.add('hidden')
+      }
+    })
   }
 }
