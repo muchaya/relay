61bafeaef5ce96fb13c8d5a8842dfc8fd57313f3
Blessing Muchaya <iblessing@outlook.com>
2026-02-23 10:06:54 +0200

    Track states better to make our ui more intuitive

--- app/javascript/controllers/location_filters_controller.js
+++ app/javascript/controllers/location_filters_controller.js
@@ -4,7 +4,7 @@ import Fuse from "fuse.js"
 // Connects to data-controller="location-filters"
 export default class extends Controller {
   
-  static targets = ["place", "leavingFromInput", "goingToInput"]
+  static targets = ["place", "leavingFromInput", "goingToInput", "goingToHint", "leavingFromHint"]
 
   static values = {
     routesGraph: Object 
@@ -21,7 +21,6 @@ export default class extends Controller {
       places.map(p => [p.id, p])
     )
 
-    // Graph indexes
     // Where can I go from this location
     // Where could I have come from if I am at this location
     this.fromIndex = {}
@@ -32,42 +31,75 @@ export default class extends Controller {
       ;(this.toIndex[r.to] ||= []).push(r.from)
     })
 
-    // Global Fuse (all places)
     this.placesFuse = new Fuse(this.places, {
       keys: ["name", "province", "locality"],
       threshold: 0.3
     })
 
-    // State
     this.selectedFromId = null
     this.selectedToId = null
   }
 
   searchFrom(event) {
     const query = event.target.value.trim()
-
+  
+    this.#invalidateFromSelection()
+  
     const places = this.selectedToId
-    ? this.originsFor(this.selectedToId)
-    : this.validOrigins()
-
+      ? this.originsFor(this.selectedToId)
+      : this.validOrigins()
+  
     this.#renderSearchResults(
-      this.searchPlaces(query, places), "leaving-from-popover"
+      this.#searchPlaces(query, places), "leaving-from-popover"
     )
+  }
+
+  leavingFromFocus() {
+    const places = this.selectedToId
+      ? this.originsFor(this.selectedToId)
+      : this.validOrigins()
+  
+    if (!this.selectedToId) {
+      this.leavingFromHintTarget.textContent = "Choose where you're departing from"
+    } else {
+      const toPlace = this.placeMap[this.selectedToId]
+      this.leavingFromHintTarget.textContent = `${places.length} places can take you to ${toPlace.name}`
+    }
+  
+    this.#renderSearchResults(places, "leaving-from-popover")
   }
 
   searchTo(event) {
     const query = event.target.value.trim()
-
+  
+    // user typed -> selection is no longer valid
+    this.#invalidateToSelection()
+  
     const places = this.selectedFromId
       ? this.destinationsFor(this.selectedFromId)
       : this.validDestinations()
-
+  
     this.#renderSearchResults(
-      this.searchPlaces(query, places), "going-to-popover"
+      this.#searchPlaces(query, places), "going-to-popover"
     )
   }
 
-  searchPlaces(query, places) {
+  goingToFocus() {
+    const places = this.selectedFromId
+      ? this.destinationsFor(this.selectedFromId)
+      : this.validDestinations()
+  
+    if (!this.selectedFromId) {
+      this.goingToHintTarget.textContent = "Choose where you're going to"
+    } else {
+      const fromPlace = this.placeMap[this.selectedFromId]
+      this.goingToHintTarget.textContent = `${places.length} destinations from ${fromPlace.name}`
+    }
+  
+    this.#renderSearchResults(places, "going-to-popover")
+  } 
+
+  #searchPlaces(query, places) {
     if (!query) return places
 
     const fuse = new Fuse(places, {
@@ -83,8 +115,7 @@ export default class extends Controller {
     const popover = result.closest(".tooltip")
   
     const placeId = result.dataset.placeId
-    const placeName =
-      result.querySelector("[data-name]")?.textContent.trim()
+    const placeName = result.querySelector("[data-name]")?.textContent.trim()
   
     if (popover.id === "leaving-from-popover") {
       this.selectedFromId = placeId
@@ -96,6 +127,23 @@ export default class extends Controller {
   
     this.dispatch("place-selected")
   }
+
+  allowedOrigins() {
+    // Leaving From input
+    // Constrained ONLY if Going To is selected
+    return this.selectedToId
+      ? this.originsFor(this.selectedToId)
+      : this.validOrigins()
+  }
+
+  allowedDestinations() {
+    // Going To input
+    // Constrained ONLY if Leaving From is selected
+    return this.selectedFromId
+      ? this.destinationsFor(this.selectedFromId)
+      : this.validDestinations()
+  }
+  
 
   destinationsFor(fromId) {
     return (this.fromIndex[fromId] || []).map(id => this.placeMap[id])
@@ -113,9 +161,18 @@ export default class extends Controller {
     return Object.keys(this.fromIndex).map(id => this.placeMap[id])
   }
 
+  #invalidateFromSelection() {
+    this.selectedFromId = null
+  }
+  
+  #invalidateToSelection() {
+    this.selectedToId = null
+  }
+
   #renderSearchResults(places, popoverId) {
     const popover = document.getElementById(popoverId)
-    popover.innerHTML = ""
+    const searchBox = popover.querySelector(".search-results")
+    searchBox.innerHTML = ""
   
     places.forEach(place => {
       const template = document.getElementById("place").content.cloneNode(true)
@@ -125,7 +182,7 @@ export default class extends Controller {
       template.querySelector("[data-type]").textContent = place.locality
       template.querySelector("[data-province]").textContent = `${place.province},`
   
-      popover.appendChild(template)
+      searchBox.appendChild(template)
     })
   }
 }
