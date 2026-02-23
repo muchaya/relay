4854b8466aabc19e57a48d51ca651c85580ae00e
Blessing Muchaya <iblessing@outlook.com>
2026-02-23 21:24:27 +0200

    Show specific route

---
+++ app/controllers/routes_controller.rb
@@ -0,0 +1,11 @@
+class RoutesController < ApplicationController
+  def index
+    @from_place = Place.find_by(name: params[:from])
+    @to_place = Place.find_by(name: params[:to])
+    
+    from_place_id = @from_place.id
+    to_place_id = @to_place.id
+
+    @route = Route.find_by(from_place_id: from_place_id, to_place_id: to_place_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "/demo", to: "static#demo"
+
+  resources :routes, only: [:index]
 end
