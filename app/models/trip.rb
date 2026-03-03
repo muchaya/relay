71aef925ee47224f4b308b4be0514f25dd82d398
Blessing Muchaya <iblessing@outlook.com>
2026-03-03 10:27:28 +0200

    Refactor trips

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,9 +1,31 @@
 class Trip < ApplicationRecord
   belongs_to :route
+  belongs_to :driver, class_name: "User"
+  belongs_to :vehicle
+
   has_many :bookings, dependent: :destroy
 
-  delegated_type :tripable, types: %w[Rideshare Bus]
+  enum status: {
+    draft: "draft",
+    active: "active",
+    full: "full",
+    completed: "completed",
+    cancelled: "cancelled"
+  }
+
+  validates :departure_time, :price, :seat_capacity, presence: true
+  validates :seat_capacity, numericality: { greater_than: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  enum :statuses, 
 
   def arrival_time
+    route.time_minutes
+
+    #departure_time + route.time_minutes.minutes
+  end
+
+  def seats_left
+    #seat_capacity - bookings.confirmed.count
   end
 end
--- db/migrate/20260224132824_create_trips.rb
+++ db/migrate/20260224132824_create_trips.rb
@@ -1,12 +1,24 @@
 class CreateTrips < ActiveRecord::Migration[8.0]
   def change
     create_table :trips do |t|
-      t.datetime :departure_time
-      t.decimal :base_price
-      t.string :status
-      t.belongs_to :route, foreign_key: true
+      t.datetime :departure_time, null: false
+      t.decimal  :price, precision: 8, scale: 2, null: false
+      t.integer :seat_capacity, null: false
+      t.boolean :women_only, default: false, null: false
+      t.string  :luggage_policy
+      t.boolean :smoking_allowed, default: false, null: false
+      t.boolean :pets_allowed, default: false, null: false
+      t.text :driver_notes
+      t.string :status, default: "active", null: false
+
+      t.references :route,  null: false, foreign_key: true
+      t.references :driver, null: false, foreign_key: { to_table: :users }
+      t.references :vehicle, null: false, foreign_key: true
 
       t.timestamps
     end
+
+    add_index :trips, :departure_time
+    add_index :trips, :status
   end
 end
